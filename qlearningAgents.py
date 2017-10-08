78d12cae2b392937a4e71601e091049355f48b04
Chua Jie Sheng <chuajiesheng@gmail.com>
2017-10-08 22:59:38 +0800

    Working but cannot win agent

--- qlearningAgents.py
+++ qlearningAgents.py
@@ -17,6 +17,9 @@ from learningAgents import ReinforcementAgent
 from featureExtractors import *
 
 import random, util, math
+from collections import deque
+import tensorflow as tf
+import numpy as np
 
 
 class QLearningAgent(ReinforcementAgent):
@@ -202,6 +205,125 @@ class ApproximateQAgent(PacmanQAgent):
                 difference = (reward + self.discount * max(look_ahead)) - self.getQValue(state, action)
 
             self.weights[f] = self.weights[f] + self.alpha * difference * v
+
+    def final(self, state):
+        "Called at the end of each game."
+        # call the super-class final method
+        PacmanQAgent.final(self, state)
+
+        # did we finish training?
+        if self.episodesSoFar == self.numTraining:
+            # you might want to print your weights here for debugging
+            "*** YOUR CODE HERE ***"
+            pass
+
+
+class TensorFlowQAgent(PacmanQAgent):
+
+    def __init__(self, extractor='IdentityExtractor', **args):
+        self.featExtractor = util.lookup(extractor, globals())()
+        PacmanQAgent.__init__(self, **args)
+        self.weights = util.Counter()
+
+        INPUT_NEURONS = 4
+        HIDDEN_NEURONS = 64 * 11 * 10
+        OUTPUT_NEURONS = 5  # action space
+        self.DIRECTIONS = [Directions.NORTH,
+                           Directions.SOUTH,
+                           Directions.EAST,
+                           Directions.WEST,
+                           Directions.STOP]
+
+        self.ip = tf.placeholder(tf.float32, [None, INPUT_NEURONS], name="ip")
+        self.op = tf.placeholder(tf.float32, [None, OUTPUT_NEURONS], name="op")
+        self.hiddenLayer = {'weights': tf.Variable(tf.random_normal([INPUT_NEURONS, HIDDEN_NEURONS])),
+                            'biases': tf.Variable(tf.random_normal([HIDDEN_NEURONS]))}
+        self.outputLayer = {'weights': tf.Variable(tf.random_normal([HIDDEN_NEURONS, OUTPUT_NEURONS])),
+                            'biases': tf.Variable(tf.random_normal([OUTPUT_NEURONS]))}
+        self.hiddenLayerOutput = tf.add(tf.matmul(self.ip, self.hiddenLayer['weights']), self.hiddenLayer['biases'])
+        self.hiddenLayerOutput = tf.nn.tanh(self.hiddenLayerOutput)
+        self.output = tf.add(tf.matmul(self.hiddenLayerOutput, self.outputLayer['weights']), self.outputLayer['biases'])
+        self.loss = tf.reduce_mean(tf.square(self.output - self.op))
+        self.loss = tf.clip_by_value(self.loss, 1e-10, 1e+20)
+        self.optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01).minimize(self.loss)
+        self.sess = tf.Session()
+        self.sess.run(tf.global_variables_initializer())
+
+        replay_memory_size = 20000
+        self.replay_memory = deque([], maxlen=replay_memory_size)
+
+    def getWeights(self):
+        return self.weights
+
+    def getQValue(self, state, action):
+        """
+          Should return Q(state,action) = w * featureVector
+          where * is the dotProduct operator
+        """
+        "*** YOUR CODE HERE ***"
+        q = 0
+        for f, v in self.featExtractor.getFeatures(state, action).items():  # use .iteritems() for python2
+            q += v * self.weights[f]
+
+        return q
+
+    def sample_memories(self, batch_size):
+        indices = np.random.permutation(len(self.replay_memory))[:batch_size]
+        samples = []
+        for idx in indices:
+            memory = self.replay_memory[idx]
+            samples.append(memory)
+        return samples
+
+    def update(self, state, action, next_state, reward):
+        """
+           Should update your weights based on transition
+        """
+        "*** YOUR CODE HERE ***"
+
+        # Add the experience to replay memory
+        value_for_state = []
+        value = [v for _, v in self.featExtractor.getFeatures(state, action).items()]
+        if len(value) == 3:
+            value_for_state.append([0] + value)
+        else:
+            value_for_state.append(value)
+
+        value_for_next_state = []
+        for nextAction in self.getLegalActions(next_state):
+            features = [v for _, v in self.featExtractor.getFeatures(next_state, nextAction).items()]
+            if len(features) == 3:
+                features = [0] + features
+            value_for_next_state.append(features)
+        done = len(self.getLegalActions(next_state)) == 0
+
+        self.replay_memory.append((value_for_state, action, reward, value_for_next_state, done))
+        # Sample sampleBatch, a batch of training data from the replay memory
+        batch_size = 50
+        sampleBatch = self.sample_memories(batch_size)
+
+        trainingInputStates = []
+        trainingTargetValues = []
+        for train_state, train_action, train_reward, train_nextState, isTerminal in sampleBatch:
+            QValues = self.sess.run([self.output], feed_dict={self.ip: np.array(train_state)})[0][0]
+            if isTerminal:
+                train_nextState = [[0, 0, 0, 0]]
+            nextStateQValues = self.sess.run([self.output], feed_dict={self.ip: np.array(train_nextState)})[0][0]
+            maxQVal = max(nextStateQValues)
+
+            # Update rule
+            if isTerminal:
+                newQVal = train_reward
+            else:
+                newQVal = (train_reward + self.discount * maxQVal)
+
+            targetQValues = QValues.copy()
+            targetQValues[self.DIRECTIONS.index(train_action)] = newQVal
+            trainingInputStates.append(train_state[0])
+            trainingTargetValues.append(targetQValues)
+
+        # Optimize
+        self.sess.run([self.optimizer], feed_dict={self.ip: trainingInputStates, self.op: trainingTargetValues})
 
     def final(self, state):
         "Called at the end of each game."
